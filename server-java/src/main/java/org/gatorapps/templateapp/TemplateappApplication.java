b5774c4276869226a449651fc0cb08b0ed58a943
Luke Li <luke.li@gatorapps.org>
2024-08-11 01:11:50 -0400

    chore: started java version

---
+++ server-java/src/main/java/org/gatorapps/templateapp/TemplateappApplication.java
@@ -0,0 +1,13 @@
+package org.gatorapps.templateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateappApplication.class, args);
+	}
+
+}
---
+++ server-java/src/test/java/org/gatorapps/templateapp/TemplateappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gatorapps.templateapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
