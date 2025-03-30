472f1efbbbfa78ddd80d366a9ae67c8ff80d681f
Eric Zhou <ericzhou001@gmail.com>
2025-03-29 22:54:39 -0400

    feat: finishing sprint3 features, only notifications left

--- client/src/views/ApplicationManagementPage/ApplicationManagementPageCard.js
+++ client/src/views/ApplicationManagementPage/ApplicationManagementPageCard.js
@@ -19,62 +19,38 @@ import { useNavigate } from 'react-router-dom';
 
 
 const GenericPageCard = () => {
-    const [applications, setApplications] = useState({ activeApplications: [], archivedApplications: [] });
+    const [applications, setApplications] = useState({ activeApplications: [], movingApplications: [], archivedApplications: [] });
     const [positions, setPositions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [selectedTab, setSelectedTab] = useState(0);
     const [selectedPosition, setSelectedPosition] = useState('');
     const [selectedApplication, setSelectedApplication] = useState(null);
-
     const navigate = useNavigate();
 
-    // Fetch positions from API
-    useEffect(() => {
-        // axiosPrivate.get('/posting/postingsList')
-        //     .then((response) => {
-        //         console.log(response.data);
-        //         if (response.data && response.data.errCode === "0" && response.data.payload.positions) {
-        //             setPositions(response.data.payload.positions);
-        //         } else {
-        //             setError("No positions found");
-        //         }
-        //     })
-        //     .catch((error) => {
-        //         setError(error.message || "Error fetching positions");
-        //     });
-        // Mock data for positions
-        const mockPositions = [
-            {
-                "positionId": "67ddeb5144c53033691a78ff",
-                "name": "Eric's Testing Position"
-            },
-            {
-                "positionId": "67ddf20744c53033691a7906",
-                "name": "Eric's Testing Position 2"
-            },
-            {
-                "positionId": "67ddf22f44c53033691a7907",
-                "name": "Eric's Testing Position 3"
-            },
-            {
-                "positionId": "67ddf24344c53033691a7908",
-                "name": "Eric's Testing Position 4"
-            }
-        ]
-        setPositions(mockPositions);
-        setSelectedPosition(mockPositions[0].positionId);
-        setLoading(false);
-    }, []);
+    const updateApplicationStatus = async (app, newStatus) => {
+        try {
+            await axiosPrivate.put(`/application/applicationStatus`, null, {
+                params: {
+                    labId: app.labId,
+                    applicationId: app.applicationId,
+                    status: newStatus
+                }
+            });
+            fetchApplications();
+        } catch (error) {
+            setError("Error updating status");
+        }
+    };
 
-    // Fetch applications when a position is selected
-    useEffect(() => {
+    const fetchApplications = async () => {
         if (selectedPosition) {
             setLoading(true);
             axiosPrivate.get(`/application/applicationManagement?positionId=${selectedPosition}`)
                 .then((response) => {
                     if (response.data && response.data.errCode === "0" && response.data.payload.applications) {
                         setApplications(response.data.payload.applications);
+                        // console.log(response.data.payload.applications);
                     } else {
                         setError("No applications found");
                     }
@@ -85,7 +61,35 @@ const GenericPageCard = () => {
                     setLoading(false);
                 });
         }
+    };
+
+    // Fetch positions from API
+    useEffect(() => {
+        axiosPrivate.get('/posting/postingsList')
+            .then((response) => {
+                // console.log(response.data);
+                if (response.data && response.data.errCode === "0" && response.data.payload.positions) {
+                    setPositions(response.data.payload.positions);
+                    setSelectedPosition(response.data.payload.positions[0].positionId);
+                } else {
+                    setError("No positions found");
+                }
+            })
+            .catch((error) => {
+                setError(error.message || "Error fetching positions");
+            });
+        setLoading(false);
+    }, []);
+
+    // Fetch applications when a position is selected
+    useEffect(() => {
+        fetchApplications();
     }, [selectedPosition]);
+
+    const handleViewApplication = (application) => {
+        console.log(application);
+        setSelectedApplication(application);
+    }
 
     return (
         <Box sx={{ width: '100%' }}>
@@ -116,50 +120,36 @@ const GenericPageCard = () => {
                             </FormControl>
                         </Box>
 
-                        {/* Application Tabs */}
+                        {/* Application Tabs (Shrink the size) */}
                         <Tabs
-                            sx={{ marginBottom: 0, height: 2 }}  // Decrease size of the tab 
-                            value={selectedTab} onChange={(e, newValue) => setSelectedTab(newValue)} centered>
-                            <Tab label="New" />
-                            <Tab label="In Progress" />
-                            <Tab label="Archived" />
+                            value={selectedTab}
+                            onChange={(e, newValue) => setSelectedTab(newValue)}
+                            centered
+                        >
+                            <Tab label="Active" sx={{ fontSize: '0.75rem' }} />
+                            <Tab label="Moving Forward" sx={{ fontSize: '0.75rem' }} />
+                            <Tab label="Archived" sx={{ fontSize: '0.75rem' }} />
                         </Tabs>
 
                         {/* Application List */}
-                        <Paper variant="outlined" sx={{ padding: '24px', marginTop: 2 }}>
+                        <Paper variant="outlined" sx={{ padding: '6px', marginTop: 2 }}>
                             {loading ? (
                                 <SkeletonGroup boxPadding={'0'} />
                             ) : error ? (
                                 <Typography color="error">{error}</Typography>
-                            ) : applications.activeApplications.length === 0 && applications.archivedApplications.length === 0 ? (
-                                <Typography variant="body1" color="textSecondary">No applications available.</Typography>
+                            ) : ([applications.activeApplications, applications.movingApplications, applications.archivedApplications][selectedTab].length === 0 ? (
+                                <Typography variant="body1" color="textSecondary" sx={{ marginBottom: '8px', padding: '16px' }}>No applications available.</Typography>
                             ) : (
-                                selectedTab === 0 ?  // New Applications
-                                    applications.activeApplications.map((app) => (
-                                        <Box key={app.opid} sx={{ marginBottom: '2px', padding: '0px', borderBottom: '1px solid #ddd' }}>
-                                            <Typography variant="h6">Student OPID: {app.opid}</Typography>
-                                            <Typography variant="body2" color="textSecondary">
-                                                Submitted: {new Date(app.submissionTimeStamp).toLocaleString()}
-                                            </Typography>
-                                        </Box>
-                                    )) : selectedTab === 1 ?  // In Progress Applications
-                                        applications.activeApplications.filter(app => app.status === 'inProgress').map((app) => (
-                                            <Box key={app.opid} sx={{ marginBottom: '16px', padding: '16px', borderBottom: '1px solid #ddd' }}>
-                                                <Typography variant="h6">Student OPID: {app.opid}</Typography>
-                                                <Typography variant="body2" color="textSecondary">
-                                                    Submitted: {new Date(app.submissionTimeStamp).toLocaleString()}
-                                                </Typography>
-                                            </Box>
-                                        )) :  // Archived Applications
-                                        applications.archivedApplications.map((app) => (
-                                            <Box key={app.opid} sx={{ marginBottom: '0px', padding: '0px', borderBottom: '1px solid #ddd' }}>
-                                                <Typography variant="h7">Student OPID: {app.opid}</Typography>
-                                                <Typography variant="body2" color="textSecondary">
-                                                    Submitted: {new Date(app.submissionTimeStamp).toLocaleString()}
-                                                </Typography>
-                                            </Box>
-                                        ))
-                            )}
+                                [applications.activeApplications, applications.movingApplications, applications.archivedApplications][selectedTab].slice(0, 3).map((app) => (
+                                    <Box key={app.opid} sx={{ marginBottom: '16px', padding: '16px', borderBottom: '1px solid #ddd' }}>
+                                        <Typography variant="h7">Applicant Name: {app.firstName} {app.lastName}</Typography>
+                                        {/* <Typography variant="h6">Student OPID: {app.opid}</Typography> */}
+                                        <Typography variant="body2" color="textSecondary">
+                                            Submitted: {new Date(app.submissionTimeStamp).toLocaleString()}
+                                        </Typography>
+                                    </Box>
+                                ))
+                            ))}
                         </Paper>
                     </Box>
                 </CardContent>
