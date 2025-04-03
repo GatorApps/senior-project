d8533df2bbf916db457ad5a8e1002da429805dfb
Luke Li <luke.li@gatorapps.org>
2025-04-02 22:13:33 -0400

    chore: set up for dev deployment

---
+++ server-java/src/main/java/org/gatorapps/garesearch/controller/FrontendController.java
@@ -0,0 +1,11 @@
+package org.gatorapps.garesearch.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FrontendController {
+    @GetMapping("/{path:[^\\.]*}")
+    public String forward() {
+        return "forward:/index.html";
+    }
+}
