be351bf65f44621eabeebab09230f9c6510fddb7
Luke Li <luke.li@gatorapps.org>
2025-04-16 03:21:31 -0400

    docs: updated readme

--- server/routes/appApi/appSettings.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const appSettingsController = require('../../controllers/appSettingsController');
-
-router.get('/userSettings', appSettingsController.getUserSettings);
-
-module.exports = router;
